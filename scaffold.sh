54ceb91c4a3e458e307d3007677da8d92d571fed
snekkalapudi <snekkalapudi@adobe.com>
2022-07-03 13:35:58 -0700

    changed install process

--- scaffold.sh
+++ scaffold.sh
@@ -35,8 +35,12 @@ ls
 apt-get update
 apt-get install awscli -y
 
+aws --version
+aws s3 ls
+
+
 mkdir -p ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/Linux_aarch64
-/usr/local/bin/aws s3 cp s3://commerce-cloud-projects/Linux_aarch64/terraform-provider-commerce-cloud ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/Linux_aarch64
+aws s3 cp s3://commerce-cloud-projects/Linux_aarch64/terraform-provider-commerce-cloud ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/Linux_aarch64
 chmod +x ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/Linux_aarch64/terraform-provider-commerce-cloud
 cat providers.tf
 terraform init
