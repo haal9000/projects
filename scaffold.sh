b1ae183b533f869ff70feca162a177ed2268f74f
snekkalapudi <snekkalapudi@adobe.com>
2022-07-03 18:16:10 -0700

    changed install process

--- scaffold.sh
+++ scaffold.sh
@@ -26,15 +26,17 @@ apt-get install awscli -y
 aws --version
 
 
-# mkdir -p ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_arm64
-# aws s3 cp s3://commerce-cloud-projects/linux_arm64/terraform-provider-commerce-cloud ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_arm64
-# chmod +x ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_arm64/terraform-provider-commerce-cloud
-# ls -al ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_arm64/
 
-mkdir -p ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64
-aws s3 cp s3://commerce-cloud-projects/linux_amd64/terraform-provider-commerce-cloud ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64
-chmod +x ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64/terraform-provider-commerce-cloud
-ls -al ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64/
+# mkdir -p ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64
+# aws s3 cp s3://commerce-cloud-projects/linux_amd64/terraform-provider-commerce-cloud ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64
+# chmod +x ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64/terraform-provider-commerce-cloud
+# ls -al ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/linux_amd64/
+
+mkdir -p ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/x86-64
+aws s3 cp s3://commerce-cloud-projects/x86-64/terraform-provider-commerce-cloud ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/x86-64
+chmod +x ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/x86-64/terraform-provider-commerce-cloud
+ls -al ~/.terraform.d/plugins/git.corp.adobe.com/jomoore/commerce-cloud/0.1/x86-64/
+
 # cat providers.tf
 terraform init
 terraform apply -var "api_token=${COMMERCE_CLOUD_TOKEN}" -var "mage_composer_username=${MAGE_COMPOSER_USERNAME}" -var "mage_composer_password=${MAGE_COMPOSER_PASSWORD}" -var "github_token=${GH_TOKEN}" -var "title=magento-cloud-auto-deploy"
